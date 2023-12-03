036737db57253fa287c5d3e3c79e72f9f4c01f2c
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2023-12-03 13:17:31 -0500

    Ready to Add Color Function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,26 +2,61 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 // localStorage.clear();
-var eventHistory = localStorage.getItem("eventHistory");
-if(eventHistory === null) {
-  eventHistory = [,,,,,,,,];
-  localStorage.setItem("eventHistory", JSON.stringify(eventHistory));
-}
-else {
-  eventHistory = JSON.parse(eventHistory);
-}
-console.log(eventHistory);
-$(".saveBtn").on("click", function () {
-  console.log("click");
-  // Logs Hour Number
-  let hourNum = $(this).parent().data("number")
-  console.log(hourNum);
-  // Logs text content
-  console.log($(this).parent().children().eq(1).val())
-});
 
-$(function() {
-  console.log("no click");
+$(function () {
+  // Logs Time and Date at the top of the page
+  let currentTime = dayjs().format('MM/DD/YYYY HH:mm');
+  $("#currentDay").text(currentTime);
+
+  // Deals with Local Storage Initialization
+  var eventHistory = localStorage.getItem("eventHistory");
+  if (eventHistory === null) {
+    eventHistory = [, , , , , , , ,];
+    localStorage.setItem("eventHistory", JSON.stringify(eventHistory));
+  }
+  else {
+    eventHistory = JSON.parse(eventHistory);
+  }
+
+  // Event Listener: Clear Calendar Button
+  $("#clearCal").on("click", function () {
+    eventHistory = [, , , , , , , ,];
+    localStorage.setItem("eventHistory", JSON.stringify(eventHistory));
+    logEvents(eventHistory);
+  })
+
+  // Event Listener: 
+  $(".saveBtn").on("click", function () {
+    console.log("click");
+    // Logs Hour Number
+    let hourNum = $(this).parent().data("number")
+    console.log("Hour Number Clicked", hourNum);
+    let currHour = Number(dayjs().format('H'));
+    console.log("Hour of the day", currHour);
+    if(hourNum > currHour) {
+      console.log("Future");
+    }
+    else if (hourNum == currHour) {
+      console.log("Present");
+    }
+    else {
+      console.log("Past")
+    }
+    // Logs text content
+    let textContent = $(this).parent().children().eq(1).val()
+    console.log(textContent);
+    eventHistory[hourNum - 9] = textContent;
+    console.log(eventHistory)
+    localStorage.setItem("eventHistory", JSON.stringify(eventHistory));
+  });
+
+
+  function logEvents(eventHistory) {
+    for (let i = 0; i < $(".description").length; i++) {
+      $($(".description")[i]).val(eventHistory[i]);
+    }
+  }
+  logEvents(eventHistory);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
